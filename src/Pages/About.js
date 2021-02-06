22d73d5842cc93cec0d1044e95678e04c9d34acc
Lia Graham <liagraham@Lias-MacBook-Pro.local>
2021-02-06 11:05:50 -0600

    updating about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -8,7 +8,6 @@ function About() {
     <Container>
       <NavBar />
       <AboutCard />
-      {console.log('about.js is working')}
     </Container>
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    {console.log('index.js is working')}
   </React.StrictMode>,
   document.getElementById('root')
 );
