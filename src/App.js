6f36f0aaa95ae4d409372fcb172de622900f3eb5
Lia Graham <liagraham@Lias-MacBook-Pro.local>
2021-02-07 15:49:24 -0600

    removing basename addition

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Contact from "./Pages/Contact";
 
 function App() {
   return (
-    <Router Basename="/*">
+    <Router Basename="/">
       <div className="App">
         <Switch>
           <Route exact path="/" component={Home} />
